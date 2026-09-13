6b29d96ffaf77ad90b90b475c1ace683530fb554
tdnguyen04 <tdnguyen04.work@gmail.com>
2026-09-13 15:57:59 -0400

    add: wire settings IPC handlers

--- src/main/ipc.js
+++ src/main/ipc.js
@@ -1,5 +1,6 @@
 const { ipcMain, BrowserWindow } = require("electron");
 const { openVaultDialog, getTree, readFile, writeFile } = require("./vault");
+const { getSettings, setSettings } = require("./settings");
 const { popupFileMenu } = require("./menu");
 
 function registerIpcHandlers() {
@@ -9,6 +10,8 @@ function registerIpcHandlers() {
   ipcMain.handle("vault:write-file", async (_event, filePath, content) =>
     writeFile(filePath, content)
   );
+  ipcMain.handle("settings:get", async () => getSettings());
+  ipcMain.handle("settings:set", async (_event, partial) => setSettings(partial));
   ipcMain.handle("menu:popup-file", (event, position = {}) => {
     const win = BrowserWindow.fromWebContents(event.sender);
     if (!win) {
